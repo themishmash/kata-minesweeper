455e3236765171178eeeeee50ca88f30afabfc4b
Michelle <wong.mich.c@gmail.com>
2020-08-13 16:18:18 +1000

    first commit

---
+++ KataMinesweeper.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace KataMinesweeper.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ KataMinesweeper/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace KataMinesweeper
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
